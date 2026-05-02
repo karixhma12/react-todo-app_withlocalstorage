8f9958d8a67b479294703560a1108012a26f8e15
Karishma <karishma.7022@gmail.com>
2026-05-02 12:05:23 +0530

    Coded the edit functionality

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -6,6 +6,8 @@ import {useRef} from "react";
 function App(){
     const [todos, setTodos] = useState([]);
     const [todoText,setTodoText] = useState("");
+    const [editingId, setEditingId] = useState(null);
+    const [editTodo,setEditTodo] = useState("");
 
     function addTodo(){
       const todo = {
@@ -30,6 +32,21 @@ function App(){
       setTodos(todosNew);
     }
 
+    function editTodoFunc(id){
+      const newTodos = todos.map((todo)=>{
+        if(id===todo.id){
+          return {...todo,text:editTodo};
+        }
+        else{
+          return todo;
+        }
+      }) 
+      
+      setEditingId(null);
+      setTodos(newTodos);  
+      
+    }
+
     return(
       <>
         <h1> Todo App </h1>
@@ -39,8 +56,26 @@ function App(){
           {todos.map((todo)=>{
             return(
               <li key={todo.id}>
-                {todo.text}
-                <button onClick={()=>{deleteTodo(todo.id)}}> Delete </button>
+
+               { (editingId===todo.id) ? 
+                  <>
+                    <input value={editTodo} onChange={e=>setEditTodo(e.target.value)}></input>
+                    <button onClick={()=>{editTodoFunc(todo.id)}}> Save </button>
+                    <button onClick={()=>setEditingId(null)}> Cancel </button>
+                  </>  
+
+                 : 
+                  <>
+                    {todo.text}
+                    <button onClick={()=>{deleteTodo(todo.id)}}> Delete </button>
+                    <button onClick = {()=>{
+                                        setEditTodo(todo.text);
+                                        setEditingId(todo.id);}}> 
+                                        Edit 
+                    </button>
+                  </>  
+                }
+                
               </li>
             )
           })}
